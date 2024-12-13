9aef53f0d246e719dd7938a2948f46107fa0bebc
Nazar Zozulya <likeemangames@gmail.com>
2024-12-14 01:27:25 +0300

    Добавил гг и отобразил его

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from os import path
+
 
 pygame.init()
 
@@ -8,9 +10,30 @@ FPS =  pygame.time.Clock()
 WIDTH = 600
 HEIGHT = 800
 
-main_display = pygame.display.set_mode((WIDTH, HEIGHT))
+img_dir = path.join(path.dirname(__file__), 'img')
 
-bg = pygame.transform.scale(pygame.image.load('img/bg.png'), (WIDTH, HEIGHT))
+bg = pygame.transform.scale(pygame.image.load(path.join(img_dir, 'bg.png')), (WIDTH, HEIGHT))
+
+main_display = pygame.display.set_mode((WIDTH, HEIGHT))
+title = pygame.display.set_caption("Pizdary game")
+
+player_width = 60
+player_height = 60
+player = pygame.transform.scale(pygame.image.load(path.join(img_dir, 'player.png')), (player_width, player_height))
+
+player_rect = player.get_rect()
+
+
+player_rect.center = (WIDTH // 2, HEIGHT *90 // 100)
+
+# class Player():
+#     x,y = 0,0
+#     red,green,blue = 0,0,0
+#     width,height = 60,60
+
+#     def draw(self):  # отрисовка
+#         pygame.draw.rect(main_display, (self.red, self.green, self.blue), (self.x, self.y, self.width, self.height))
+
 
 
 
@@ -22,4 +45,6 @@ while playing:
             playing = False
     
     main_display.blit(bg, (0, 0))
+
+    main_display.blit(player, player_rect)
     pygame.display.update()
